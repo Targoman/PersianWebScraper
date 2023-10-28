d91b3f962705073a8cb0193052ceb2488260a577
Pooya-Y <54044959+Pooya-Y@users.noreply.github.com>
2023-10-28 15:33:36 +0330

    tebyan

--- test/index.js
+++ test/index.js
@@ -339,6 +339,9 @@ clear; rm -rf .db corpora; yarn dev && yarn start shoaresal -u https://shoaresal
 clear; rm -rf .db corpora; yarn dev && yarn start bankdariirani -u https://bankdariirani.ir/fa/news/213586/تحقق-سود-بالای-شرکت-با-مدیریت-ریسک-و-سبد-سرمایه-گذاری
 clear; rm -rf .db corpora; yarn dev && yarn start bankdariirani -u https://bankdariirani.ir/fa/tags/4321/1/نشست-خبری
 
+clear; rm -rf .db corpora; yarn dev && yarn start tebyan -u https://article.tebyan.net/483979
+clear; rm -rf .db corpora; yarn dev && yarn start tebyan -u https://image.tebyan.net/Image/428718/
+
 ./rundocker.sh khamenei query "SELECT id,url FROM tblURLs WHERE status = 'F' AND url NOT LIKE '%/tag%' AND url NOT LIKE '%/rss%' AND url NOT LIKE '%/subject/%' AND url NOT LIKE '%/archive%' AND url NOT LIKE '%/service/%' AND url NOT LIKE '%/topic/%' AND url NOT LIKE '%/redirect/%' AND url NOT LIKE '%/taxonomy/%' AND url NOT LIKE '%/comment/%' AND url NOT LIKE '%/Invalid/%' AND url NOT LIKE '%/others-index%' AND url NOT LIKE '%/speech?nt=%' AND url NOT LIKE '%/newspart-index%' AND url NOT LIKE '%/%-index%'  AND url NOT LIKE '%/book-archive%' AND url NOT LIKE '%/search%' AND url NOT LIKE '%/roadmap%' AND url NOT LIKE '%/mobile%'  AND id > 100  LIMIT 100"
 
 
