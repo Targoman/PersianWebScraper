632043c7ab41ba88b5392779ea130b63a46b6668
Pooya-Y <54044959+Pooya-Y@users.noreply.github.com>
2023-10-29 18:14:38 +0330

    lioncomputer

--- test/index.js
+++ test/index.js
@@ -373,6 +373,9 @@ clear; rm -rf .db corpora; yarn dev && yarn start persiantools -u https://forum.
 clear; rm -rf .db corpora; yarn dev && yarn start majidonline -u https://forum.majidonline.com/threads/unreal-engine.219617/
 clear; rm -rf .db corpora; yarn dev && yarn start majidonline -u https://forum.majidonline.com/threads/201318
 
+clear; rm -rf .db corpora; yarn dev && yarn start lioncomputer -u https://forum.lioncomputer.com/topic/131247-مشاوره-خريد-مودم-مبين-نت/
+clear; rm -rf .db corpora; yarn dev && yarn start lioncomputer -u https://forum.lioncomputer.com/topic/131247-مشاوره-خريد-مودم-مبين-نت/page/2/
+
 ./rundocker.sh khamenei query "SELECT id,url FROM tblURLs WHERE status = 'F' AND url NOT LIKE '%/tag%' AND url NOT LIKE '%/rss%' AND url NOT LIKE '%/subject/%' AND url NOT LIKE '%/archive%' AND url NOT LIKE '%/service/%' AND url NOT LIKE '%/topic/%' AND url NOT LIKE '%/redirect/%' AND url NOT LIKE '%/taxonomy/%' AND url NOT LIKE '%/comment/%' AND url NOT LIKE '%/Invalid/%' AND url NOT LIKE '%/others-index%' AND url NOT LIKE '%/speech?nt=%' AND url NOT LIKE '%/newspart-index%' AND url NOT LIKE '%/%-index%'  AND url NOT LIKE '%/book-archive%' AND url NOT LIKE '%/search%' AND url NOT LIKE '%/roadmap%' AND url NOT LIKE '%/mobile%'  AND id > 100  LIMIT 100"
 
 
