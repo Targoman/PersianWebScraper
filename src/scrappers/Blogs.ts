3a06da64ae3ce6b9d0ad3218113ff25081e3592f
Mehran Ziabary <ziabary@targoman.com>
2023-10-31 14:53:58 +0330

    blog renamed to blogir

--- src/scrappers/Blogs.ts
+++ src/scrappers/Blogs.ts
@@ -42,40 +42,6 @@ export class virgool extends clsScrapper {
     }
 }
 
-export class blogir extends clsScrapper {
-    constructor() {
-        super(enuDomains.blogir, "blog.ir", {
-            selectors: {
-                article: (page: HTMLElement) =>
-                    (page.querySelectorAll("#block-post") ||
-                        page.querySelectorAll(".post")
-                    ).length > 1
-                        ? page.querySelector("NOArticle")
-                        : (page.querySelector("#block-post") || page.querySelector(".post")),
-                title: ".title, .post-title",
-                content: {
-                    main: ".body .cnt>*, .post-full-content>*, .post-matn>*, .post-content",
-                    ignoreNodeClasses: ["fw-900"]
-                },
-                tags: (article: HTMLElement) => article.querySelectorAll('a').filter(el => el.getAttribute("href")?.startsWith("/tag")),
-                datetime: {
-                    conatiner: ".post_date, .post-details-date, .date_title, .post-detail-right"
-                },
-                comments: {
-                    container: (article: HTMLElement) => article.querySelectorAll(".cm-body, .cm-reply-main, .comment-reply"),
-                    author: ".cm-name",
-                    datetime: ".post-detail-top, .comment-date",
-                    text: ".comment-matn, .comment-body, .comment-reply-body, .comment-reply"
-                }
-            },
-        })
-    }
-
-    normalizePath(url: URL) {
-        return url.toString()
-    }
-}
-
 export class ninisite extends clsScrapper {
     constructor() {
         super(enuDomains.ninisite, "ninisite.com", {
@@ -299,9 +265,9 @@ export class yekpezeshk extends clsScrapper {
     }
 }
 
-export class blog extends clsScrapper {
+export class blogir extends clsScrapper {
     constructor() {
-        super(enuDomains.blog, "blog.ir", {
+        super(enuDomains.blogir, "blog.ir", {
             selectors: {
                 article: ".block-post, .post.post_detail, .post, .post-container",
                 title: "h2, h3",
