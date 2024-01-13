6ddcf317d0f201888f62dd8a0de1c91dc0af365a
Pooya-Y <54044959+Pooya-Y@users.noreply.github.com>
2024-01-13 15:34:22 +0330

    divar

--- test/index.js
+++ test/index.js
@@ -1115,6 +1115,8 @@ clear; rm -rf .db corpora; yarn dev && yarn start meghdadit -u https://meghdadit
 clear; rm -rf .db corpora; yarn dev && yarn start sheypoor -u https://blog.sheypoor.com/cars-with-m15-engine/
 clear; rm -rf .db corpora; yarn dev && yarn start sheypoor -u https://blog.sheypoor.com/buying-cars-from-100-to-300-million/
 
+clear; rm -rf .db corpora; yarn dev && yarn start divar -u https://divar.ir/v/آپارتمان-۲خواب-فول-امکانات-پروژه-ارغوان/QZaCe9yK
+
 ./rundocker.sh khamenei query "SELECT id,url FROM tblURLs WHERE status = 'F' AND url NOT LIKE '%/tag%' AND url NOT LIKE '%/rss%' AND url NOT LIKE '%/subject/%' AND url NOT LIKE '%/archive%' AND url NOT LIKE '%/service/%' AND url NOT LIKE '%/topic/%' AND url NOT LIKE '%/redirect/%' AND url NOT LIKE '%/taxonomy/%' AND url NOT LIKE '%/comment/%' AND url NOT LIKE '%/Invalid/%' AND url NOT LIKE '%/others-index%' AND url NOT LIKE '%/speech?nt=%' AND url NOT LIKE '%/newspart-index%' AND url NOT LIKE '%/%-index%'  AND url NOT LIKE '%/book-archive%' AND url NOT LIKE '%/search%' AND url NOT LIKE '%/roadmap%' AND url NOT LIKE '%/mobile%'  AND id > 100  LIMIT 100"
 
 
